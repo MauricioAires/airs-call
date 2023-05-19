1165e3e51196ae382d03e76975b0c9948741b9fc
MauricioAires <mauricioaires015@gmail.com>
2023-05-19 11:31:48 -0300

    feat(document) configure stitches in server-side rendering mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { getCssText } from "@airs-ui/react";
 import { Head, Html, Main, NextScript } from "next/document";
 
 
@@ -8,6 +9,11 @@ export default function Document(){
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
+
+   <style id="stitches" dangerouslySetInnerHTML={{
+    __html: getCssText()
+   }}/>
+
     </Head>
     <body>
         <Main />
