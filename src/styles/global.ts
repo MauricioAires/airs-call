9d156f634da8e494052987c0081a9ea00d64dae3
MauricioAires <mauricioaires015@gmail.com>
2023-05-19 11:27:33 -0300

    feat(app) config styles

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { globalCss } from '@airs-ui/react'
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    padding: 0,
+    margin: 0
+  },
+  body : {
+    backgroundColor: '$gray900',
+    color: '$gray100',
+    '-webkit-font-smoothing': 'antialiased'
+  }
+})
