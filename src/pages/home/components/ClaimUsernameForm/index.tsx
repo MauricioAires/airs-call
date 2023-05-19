2195fe707bd71a06ec5ea383f9f50f0ce2d78192
MauricioAires <mauricioaires015@gmail.com>
2023-05-19 17:25:08 -0300

    feat(user) username initial value

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -5,6 +5,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import * as S from './styles'
+import { useRouter } from 'next/router'
 
 const claimUserFormSchema = z.object({
   username: z
@@ -21,16 +22,20 @@ const claimUserFormSchema = z.object({
 type ClaimUsernameFormData = z.infer<typeof claimUserFormSchema>
 
 export function ClaimUsernameForm() {
+  const router = useRouter()
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUserFormSchema),
   })
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
-    console.log(data)
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
   }
   return (
     <S.Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
@@ -42,7 +47,7 @@ export function ClaimUsernameForm() {
         {...register('username')}
       />
 
-      <Button size="sm" type="submit">
+      <Button size="sm" type="submit" disabled={isSubmitting}>
         <ArrowRight /> Reservar
       </Button>
       <S.FormAnnotation>
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,6 +5,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 import * as S from './styles'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const registerFormSchema = z.object({
   username: z
@@ -27,10 +29,23 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      username: 'mau',
+      name: '',
+    },
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data)
