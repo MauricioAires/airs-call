c2fd28b3dec0125fbf51bc752a1becfbeec66813
MauricioAires <mauricioaires015@gmail.com>
2023-05-19 15:34:31 -0300

    feat(claim username form) config zod and react-hook-form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,12 +1,31 @@
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 import { Button, TextInput } from '@airs-ui/react'
+import { z } from 'zod'
 
 import * as S from './styles'
-import { ArrowRight } from 'phosphor-react'
+
+const claimUserFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUserFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
   return (
-    <S.Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+    <S.Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        autoComplete="off"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         <ArrowRight /> Reservar
       </Button>
