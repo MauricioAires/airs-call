11c59a57ea11981bea47e2b41c2e1a742671fb5a
MauricioAires <mauricioaires015@gmail.com>
2023-05-24 14:37:30 -0300

    feat(auth config) GoogleProvider scope

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -6,7 +6,33 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ account }) {
+      console.log(account?.scope)
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        // É o mesmo que false só que direcionando para um lugar
+        return '/register/connect-calendar/?error=permissions'
+      }
+
+      // tudo certo
+      return true
+    },
+  },
 }
 export default NextAuth(authOptions)
+
+// https://www.googleapis.com/auth/userinfo.email => Veja o endereço de e-mail principal da sua Conta do Google
+// https://www.googleapis.com/auth/userinfo.profile => Veja suas informações pessoais, incluindo qualquer informação pessoal que você disponibilizou publicamente
+
+// NOTE: Apenas o usuário pode realizar modificações näo nenhum acesso administrativo que possa ter acesso
+// https://www.googleapis.com/auth/calendar => Veja, edite, compartilhe e exclua permanentemente todos os calendários que você pode acessar usando o Google Agenda
