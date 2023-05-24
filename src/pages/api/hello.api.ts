6dd432c76c74a17aa47d4e40a01c31a263145906
MauricioAires <mauricioaires015@gmail.com>
2023-05-24 10:01:25 -0300

    feat(register) create user with axios and prisma

--- src/pages/api/hello.api.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handle(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({
-    name: 'john Doe',
-  })
-}
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { z } from 'zod'
 import * as S from './styles'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -48,7 +49,14 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
