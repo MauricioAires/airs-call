677d319749907bee468e4a0560d1376f8f4982e9
MauricioAires <mauricioaires015@gmail.com>
2023-06-02 16:29:50 -0300

    feat(api) update profile bio

---
+++ src/pages/api/users/profile.api.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+const updateProfileBodySchema = z.object({
+  bio: z.string(),
+})
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'PUT') {
+    return res.status(405).end()
+  }
+
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  // Não autenticado
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { bio } = updateProfileBodySchema.parse(req.body)
+
+  await prisma.user.update({
+    where: {
+      id: session.user.id,
+    },
+    data: {
+      bio,
+    },
+  })
+
+  return res.status(204).end()
+}
