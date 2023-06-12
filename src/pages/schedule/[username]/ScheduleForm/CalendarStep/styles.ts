94b9e09c1740e0cd85f84c592ec35928f0aab25a
MauricioAires <mauricioaires015@gmail.com>
2023-06-12 14:12:50 -0300

    feat(schedule form) calendar step

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -0,0 +1,11 @@
+import { Box, styled } from '@airs-ui/react'
+
+export const Container = styled(Box, {
+  margin: '$6 auto 0',
+  padding: 0,
+  display: 'grid',
+  position: 'relative',
+
+  width: 540,
+  gridTemplateColumns: '1fr',
+})
