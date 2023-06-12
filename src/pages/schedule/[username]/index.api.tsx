4a1e29b05c36f1066727c9da962c65564015e29e
MauricioAires <mauricioaires015@gmail.com>
2023-06-12 11:20:05 -0300

    feat(user schedule) create base page

---
+++ src/pages/schedule/[username]/index.api.tsx
@@ -0,0 +1,59 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { Avatar, Heading, Text } from '@airs-ui/react'
+import { prisma } from '@/lib/prisma'
+
+import * as S from './styles'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <S.Container>
+      <S.UserHeader>
+        <Avatar src={user.avatarUrl} alt={user.name} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </S.UserHeader>
+    </S.Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    fallback: 'blocking',
+    paths: [],
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
