da9ee559ef563122db6b020572dc6f0020242904
MauricioAires <mauricioaires015@gmail.com>
2023-06-15 15:35:41 -0300

    feat(api) blocked dates busy days

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -46,7 +46,28 @@ export default async function handle(
     )
   })
 
+  const blockedDateRaw: Array<{ date: number }> = await prisma.$queryRaw`
+  SELECT x.date FROM (
+    SELECT
+    EXTRACT(DAY FROM s.date) AS date,
+    COUNT(s.id) AS amount,
+    (( uti.time_end_in_minutes - uti.time_start_in_minutes) / 60) as size
+    FROM schedulings as s
+
+    LEFT JOIN user_time_intervals AS uti
+    ON uti.week_day = WEEKDAY(DATE_ADD(s.date, INTERVAL 1 DAY))
+
+    WHERE s.user_id = ${user.id}
+        AND DATE_FORMAT(s.date, "%Y-%m") = ${`${year}-${month}`}
+
+    GROUP BY EXTRACT(DAY FROM s.date), size
+
+    HAVING amount >= size
+  ) AS x
+  `
+
   return res.send({
     blockedWeekDays,
+    blockedDates: blockedDateRaw.map((date) => date.date),
   })
 }
